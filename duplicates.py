07d95ca735fce72e4b7d84c1226186c5d2143163
Ranc58 <rancvova@gmail.com>
2017-05-16 10:40:24 +0300

    Refactoring

--- duplicates.py
+++ duplicates.py
@@ -1,23 +1,25 @@
 import os
 
 
-def copies_search(way_to_dir):
-    dublicate_files_list = []
-    full_ways=[]
-    for root, dirs, files in os.walk(way_to_dir):
+def create_list_of_files_and_sizes(way_to_dir):
+    testing_list = []
+    for root, dirs, files, in os.walk(way_to_dir):
         for file in files:
-            dublicate_files_list.append(file)
-            full_ways.append(os.path.join(root,file))
-    ssss = dict((x, dublicate_files_list.count(x)) for x in set(dublicate_files_list) if dublicate_files_list.count(x) > 1 )
-    for value,key in ssss.items():
-        print('Файл {0} повторяется {1} раза.'.format(value,key))
-    print(full_ways)
-    #for name in full_ways:
-    #    print(os.path.getsize(name))
-    print(dublicate_files_list)
-    print(ssss)
+            file_size = os.path.getsize(os.path.join(root, file))
+            testing_list.append("{} имеющий размер: {} мегабайт".format(file, ("%.4f" % (file_size / 2 ** 20))))
+    return testing_list
+
+
+def copies_search():
+    dict_of_doubles = dict((x, testing_list.count(x)) for x in set(testing_list) if testing_list.count(x) > 1)
+    for value, key in dict_of_doubles.items():
+        print('Файл {0} повторяется {1} раза.'.format(value, key))
+    print(dict_of_doubles)
+
+def copies_print():
+    pass
 
 
 if __name__ == '__main__':
     # way_to_dir = input('Please enter way to directory: ')
-    copies_search('D:\\books')
+    create_list_of_files_and_sizes('D:\\books')
